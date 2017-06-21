939f3dac69c5146855876f8ecd0b169f2bfb0866
Nathan Duprey <nathanduprey@hotmail.com>
2017-06-20 21:36:12 -0400

    changed config file type to be compatible with configparser

--- config.py
+++ config.py
@@ -34,62 +34,78 @@ def readConfig():
                 statsFile = line[1].strip() + '.csv'
                 elif
 
-configText = ''';------------------------------------------------------------------------------;
-; This is the config file for trainbot. Various parameters need to be          ;
-; configured here in order for the bot to work. Read the description for each  ;
-; parameter to find our what it does and the required information. Lines       ;
-; starting with ; are viewed as comments and will not be parsed.               ;
-;------------------------------------------------------------------------------;
+configText = '''################################################################################
+# This is the config file for trainbot. Various parameters need to be          #
+# configured here in order for the bot to work. Read the description for each  #
+# parameter to find our what it does and the required information. Lines       #
+# starting with # are viewed as comments and will not be parsed.               #
+################################################################################
 
-;------------------------------------------------------------------------------;
-; OWNER_ID is the ID of the bot's owner. They will have full access to all     ;
-; commands. If this is not you, be careful who you grant it to                 ;
-;------------------------------------------------------------------------------;
+[Owner]
+################################################################################
+# OWNER_ID is the ID of the bot's owner. They will have full access to all     #
+# commands. If this is not you, be careful who you grant it to                 #
+################################################################################
 OWNER_ID = your_id_here
 
-;------------------------------------------------------------------------------;
-; BOT_TOKEN is the token to be used to login as a bot account. This is         ;
-; available when creating a Bot User on the Developer site here:               ;
-; https://discordapp.com/developers/applications/me                            ;
-;------------------------------------------------------------------------------;
+[Credentials]
+################################################################################
+# BOT_TOKEN is the token to be used to login as a bot account. This is         #
+# available when creating a Bot User on the Developer site here:               #
+# https://discordapp.com/developers/applications/me                            #
+################################################################################
 BOT_TOKEN = bot_token_here
 
-;------------------------------------------------------------------------------;
-; CMD_PREFIX is the character used to determine what will trigger the bot.     ;
-; Example:                                                                     ;
-;   CMD_PREFIX = !                                                             ;
-;   !addplayer                                                                 ;
-;------------------------------------------------------------------------------;
+[Chat]
+################################################################################
+# CMD_PREFIX is the character used to determine what will trigger the bot. If  #
+# you only want the bot to listen and send messages in a specific text         #
+# channel, uncomment LISTEN_CHANNEL and provide the chanel's ID.               #
+################################################################################
 CMD_PREFIX = !
+# LISTEN_CHANNEL =
 
-;------------------------------------------------------------------------------;
-; STATS_FILE is the name of the file used for storing the stats.               ;
-;------------------------------------------------------------------------------;
+[Files]
+################################################################################
+# STATS_FILE is the name of the file used for storing the stats.               #
+################################################################################
 STATS_FILE = stats
 
-;------------------------------------------------------------------------------;
-; DEFAULT_CMDS and DEFAULT_ROLE control what commands are available by         ;
-; default. The commands listed after DEFAULT_CMDS are grated to user with      ;
-; DEFAULT_ROLE or @everyone if no role is provided. DEFAULT_ROLE expects the   ;
-; ID of the role. Commands should be seperated by a space.                     ;
-;------------------------------------------------------------------------------;
-DEFAULT_CMDS = stats help
-DEFAULT_ROLE = 131586834642894849
 
-;------------------------------------------------------------------------------;
-; ADMIN_CMDS and ADMIN_ROLE function the same as DEFAULT_CMDS and DEFAULT_ROLE ;
-; but are used for setting permissions for commands that should only be used   ;
-; by trusted users or roles. It is a good idea to restrict the addplayer,      ;
-; removeplayer, and setplayer commands to this.                                ;
-;------------------------------------------------------------------------------;
-ADMIN_CMDS = addplayer removeplayer setplayer
-ADMIN_ROLE = 319626374241320973
 
-;------------------------------------------------------------------------------;
-; UPDATE_DB_ROLE functions the same as DEFAULT_ROLE and ADMIN_ROLE but is      ;
-; provided to allow the choochoo command to be used by roles without access to ;
-; admin commands. Set this to the same as ADMIN_ROLE if you want the same      ;
-; restriction on it.                                                           ;
-;------------------------------------------------------------------------------;
-UPDATE_DB_ROLE = 319626374241320973
+################################################################################
+#                                 PERMISSIONS                                  #
+#                                                                              #
+# Permissions can be configured in any way desired and are configured in       #
+# groups. To create a permission group, copy the section below and change the  #
+# name of the header contained in the [square brackets].                       #
+#                                                                              #
+# [Group Name]                                                                 #
+# CMDS = command1 command1                                                     #
+# ROLES = 112233445566778899 998877665544332211                                #
+# USERS = 102030405060708090                                                   #
+#                                                                              #
+# The commands listed after CMDS are the commands available to the group. The  #
+# role IDs listed after ROLES are the server roles that will be granted CMDS.  #
+# If you want to override a role for a specific user, list their ID after      #
+# USERS. There are a couple groups already created below as examples. You are  #
+# free to modify them as needed but do not remove the Default group.           #
+################################################################################
+
+# Do not rename or delete this role. These permissions are assigned to anyone
+# who doesn't get assigned to another group.
+[Default]
+CMDS = help
+# ROLES =
+# USERS =
+
+[Members]
+CMDS = stats help
+ROLES = 131586834642894849
+# USERS =
+
+[Mods]
+CMDS = choochoo addplayer stats help
+ROLES = 319626374241320973
+# USERS =
 '''
